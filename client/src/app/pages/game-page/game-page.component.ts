52d803813945b4eb4d57c1d5a2ba398c153dd05d
irmax-poly <irina.maximciuc@polymtl.ca>
2025-02-08 14:35:35 -0500

    code de depart

---
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+
+@Component({
+    selector: 'app-game-page',
+    standalone: true,
+    templateUrl: './game-page.component.html',
+    styleUrls: ['./game-page.component.scss'],
+    imports: [SidebarComponent, PlayAreaComponent],
+})
+export class GamePageComponent {}
